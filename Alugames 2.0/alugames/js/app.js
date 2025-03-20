92107aaaa14865154822f4adfe8fdc5f10e1d90c
André Felipe <andrefelipealecrim@gmail.com>
2025-03-20 01:26:21 -0300

    Confirmação para devolução e contador de jogos alugados

--- Alugames 2.0/alugames/js/app.js
+++ Alugames 2.0/alugames/js/app.js
@@ -1,12 +1,20 @@
+let contador = 1;
+
 function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     if (game.querySelector('a').classList.contains('dashboard__item__button--return')) {
-        game.querySelector('a').classList.remove('dashboard__item__button--return');
-        game.querySelector('a').textContent = 'Alugar';
-        game.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented');
+        if (window.confirm('Você tem certeza que deseja devolver ?')) {
+            game.querySelector('a').classList.remove('dashboard__item__button--return');
+            game.querySelector('a').textContent = 'Alugar';
+            game.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented');
+            contador--
+        }
+        
     } else {
         game.querySelector('a').classList.add('dashboard__item__button--return');
         game.querySelector('a').textContent = 'Devolver';
         game.querySelector('.dashboard__item__img').classList.add('dashboard__item__img--rented');
+        contador++
     }
+    console.log(contador);
 }
